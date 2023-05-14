531917d2e34470e9b2852b2fbbf0dab9ced2a070
Matthew Del Monte <mdel3089@gmail.com>
2023-05-14 16:16:33 -0700

    added createChatCompletion message and received response from chat-gpt

---
+++ chatbot/node/index.js
@@ -0,0 +1,20 @@
+import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+config();
+
+console.log(process.env.OPENAI_API_KEY);
+
+const configuration = new Configuration({
+  organization: "org-PkdEIY2TP25MB5UfQlPr9dgx",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+openai
+  .createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello Chat-GPT" }],
+  })
+  .then((res) => {
+    console.log(res.data.choices);
+  });
