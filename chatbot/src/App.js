9c3fe6364ddbd86ccff529d37c0afade2cadc68f
Matthew Del Monte <mdel3089@gmail.com>
2023-05-22 16:06:22 -0700

    refined styling and added simulated error functionality

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -4,6 +4,7 @@ import ChatBox from "./components/chatBox";
 function App() {
   return (
     <div className="App">
+      <h1 className="App-header">myVIP AI Chatbot POC</h1>
       <ChatBox />
     </div>
   );
--- chatbot/src/components/chatBox/index.js
+++ chatbot/src/components/chatBox/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Configuration, OpenAIApi } from "openai";
 import "./index.css";
 
@@ -11,9 +11,28 @@ function ChatBox() {
   const openai = new OpenAIApi(config);
 
   const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
+  const [results, setResults] = useState([]);
   const [error, setError] = useState("");
+  const [consoleError, setConsoleError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [chatVisible, setChatVisible] = useState(false); // Added a new state for handling visibility of the chatbox
+
+  useEffect(() => {
+    const errorHandler = (error) => {
+      setConsoleError(error.message);
+    };
+
+    window.addEventListener("error", errorHandler);
+    return () => {
+      window.removeEventListener("error", errorHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (consoleError) {
+      setPrompt(`How can I fix this error: ${consoleError}`);
+    }
+  }, [consoleError]);
 
   const handleChange = async () => {
     setLoading(true);
@@ -21,12 +40,13 @@ function ChatBox() {
       // add a 3 second delay
       await new Promise((resolve) => setTimeout(resolve, 3000));
       const response = await openai.createCompletion({
-        model: "text-davinci-001",
+        // model: "babbage-code-search-code", // code writing model
+        model: "text-davinci-001", // text writing model
         prompt: prompt,
         temperature: 0.2,
         max_tokens: 350,
       });
-      setResult(response.data.choices[0].text);
+      setResults([response.data.choices[0].text, ...results]);
       setLoading(false);
     } catch (error) {
       setError(error);
@@ -41,30 +61,62 @@ function ChatBox() {
     handleChange();
   };
 
+  const handleChatToggle = () => {
+    setChatVisible(!chatVisible); // Toggle the visibility of the chatbox
+  };
+
+  function simulateError() {
+    try {
+      let obj;
+      console.log(obj.property); // obj is undefined, so this will throw an error
+    } catch (error) {
+      setConsoleError(`Error at line 68, column 12: ${error.message}`);
+    }
+  }
+
   return (
-    <div className="chat-container">
-      <h2 className="chat-header">Personal ChatGPT</h2>
-      <textarea
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-        className="chat-input"
-        cols="30"
-        rows="10"
-        placeholder="Hello there! How may I assist you today?"
-      />
-
-      <button className="chat-button" onClick={handleClick}>
-        {loading ? "Generating..." : "Generate"}
+    <>
+      <button className="chat-simulate-error" onClick={simulateError}>
+        Simulate Error
       </button>
+      {!chatVisible && (
+        <button className="chat-toggle" onClick={handleChatToggle}>
+          +
+        </button>
+      )}
+      {chatVisible && (
+        <div className="chat-container">
+          <button className="chat-toggle" onClick={handleChatToggle}>
+            -
+          </button>
+          <h2 className="chat-header">Personal ChatGPT</h2>
+          <textarea
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            className="chat-input"
+            cols="30"
+            rows="10"
+            placeholder="Hello there! How may I assist you today?"
+          />
 
-      {result ? (
-        <div className="chat-result">{result}</div>
-      ) : error ? (
-        <div className="chat-error">
-          <p>Request exceed {error.message}</p>
+          <button className="chat-button" onClick={handleClick}>
+            {loading ? "Generating..." : "Generate"}
+          </button>
+
+          {results.length > 0 ? (
+            <textarea
+              value={results.join("\n")}
+              className="chat-result"
+              readOnly
+            />
+          ) : error ? (
+            <div className="chat-error">
+              <p>Request exceed {error.message}</p>
+            </div>
+          ) : null}
         </div>
-      ) : null}
-    </div>
+      )}
+    </>
   );
 }
 
